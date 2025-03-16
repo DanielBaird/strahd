c0a3a5965b82689cc59c4ec6a02d40173a865ee5
Daniel Baird <daniel.baird@jcu.edu.au>
2025-03-16 21:08:27 +1000

    adds index page

--- compile.sh
+++ compile.sh
@@ -2,14 +2,20 @@ scriptname=$(realpath "$0")
 rootdir="$(dirname "$scriptname")"
 inputdir="$rootdir/sessions"
 outputdir="$rootdir/docs"
+indexfile="$outputdir/index.html"
 style="${2-sessionnotes}"
+
+# get the index started
+cat "$rootdir/html/generic/preall.html" > "$indexfile"
+cat "$rootdir/html/styles/preindex.html" > "$indexfile"
 
 for mdfile in "$inputdir"/*.md; do
 
     # mdfile="$1"
     # htmlfile="$1.html"
     basefile="$(basename "$mdfile")"
-    htmlfile="$outputdir/$basefile.html"
+    htmlfilename="$basefile.html"
+    htmlfile="$outputdir/$htmlfilename"
 
     if [ ! -f "$mdfile" ]; then
         # specified markdown file doesn't exist --------------------
@@ -17,15 +23,21 @@ for mdfile in "$inputdir"/*.md; do
 
     else
         # everything seems fine ------------------------------------
-        echo "using the $style style on $mdfile ..."
+        echo "using the $style style on $basefile ..."
 
         cat "$rootdir/html/generic/preall.html" > "$htmlfile"
         cat "$rootdir/html/styles/pre$style.html" >> "$htmlfile"
         marked -i "$mdfile" >> "$htmlfile"
         cat "$rootdir/html/generic/postall.html" >> "$htmlfile"
 
-        echo "...created file:///`wslpath -m "$htmlfile"` "
+        echo "<li><a href=\"$htmlfilename\">$htmlfilename</a></li>" >> "$indexfile"
+
+        echo "...created $htmlfilename at file:///`wslpath -m "$htmlfile"` "
 
     fi
 
 done # finished for loop though md files in ./sessions
+
+# finish the index
+cat "$rootdir/html/styles/postindex.html" > "$indexfile"
+cat "$rootdir/html/generic/postall.html" > "$indexfile"
